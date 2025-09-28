ec87f9cd9b52df4376aa3026ccb4b15e96c95c20
LCA <73611501+AZ-LCA@users.noreply.github.com>
2025-09-28 13:33:31 -0400

    updated our parser to work with the new official byte format

--- backend/file_parser.py
+++ backend/file_parser.py
@@ -1,24 +1,40 @@
 import serial # pyserial
 import time
-import backend.global_vars as gv
+import global_vars as gv
+
+# [0] hardcoded sanity assert value (0xbb)
+# [1] CAN device id
+# [2] Subidentifier (for devices that send more than one type of data per address, i.e. from BMS AUX (0x7D): 0x00 = low cell V, 0x01 = high cell V, etc.)
+# [3-6] timestamp, in ms from device enable
+# [7-14*] data, big-endian? (i need to double check the endianness but memcpy gives the correct result either way)
+# [15*] hardcoded sanity assert value (0x9a)
+# So 16 bytes / entry gives us a lot of wiggle room for the amt of data we send over
 
 def parse_in(inp):
     if type(inp) == bytes:
         inp = ''.join(f'{byte:08b}' for byte in inp)
+        # print(inp)
     if type(inp) == str:
         inp = int(inp, 2)
     if type(inp) == int or type(inp) == float:
-        inp = inp - ((inp >> 112) << 112)
-        p = inp & 1 # right then left shift
-        data = (inp - ((inp >> 65) << 65) - p) >> 1
-        time = (inp - ((inp >> 97) << 97) - p - (data << 1)) >> 65
-        typ = (inp - ((inp >> 100) << 100) - p - (data << 1) - (time << 65)) >> 97
-        dev_addr = (inp - p - (data << 1) - (time << 65) - (typ << 97)) >> 100
+        # inp = inp - ((inp >> 112) << 112)
+        # p = inp & 1 # right then left shift
+        # data = (inp - ((inp >> 65) << 65) - p) >> 1
+        # time = (inp - ((inp >> 97) << 97) - p - (data << 1)) >> 65
+        # typ = (inp - ((inp >> 100) << 100) - p - (data << 1) - (time << 65)) >> 97
+        # dev_addr = (inp - p - (data << 1) - (time << 65) - (typ << 97)) >> 100
+        inp = inp - ((inp >> 128) << 128)
+        hcSanValB = inp & 0xFF 
+        data = (inp - ((inp >> 72) << 72) - hcSanValB) >> 8
+        timestamp = (inp - ((inp >> 104) << 104) - hcSanValB - (data << 8)) >> 72
+        subId = (inp - ((inp >> 112) << 112) - hcSanValB - (data << 8) - (timestamp << 72)) >> 104
+        canId = (inp - ((inp >> 120) << 120) - hcSanValB - (data << 8) - (timestamp << 72) - (subId << 104)) >> 112
+        hcSanValA = inp >> 120
     else: 
         print(type(inp))
         
     
-    return dev_addr, typ, time, data, p
+    return hcSanValA, canId, subId, timestamp, data, hcSanValB
     
 # dev_addr, typ, time, data, p = parse_in("0000000000000000000001000000000001001000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000001")
 # print(dev_addr)
@@ -28,12 +44,15 @@ def parse_in(inp):
 # print(p)
 
 def read_bin_file(file):
-    data
     with open(file, 'rb') as f:
-        for line in f:
-            dev_addr, typ, time, data, p = parse_in(line)
-            print(dev_addr, typ, time, data, p)
-            row = [dev_addr, typ, time, data, p]
+        while True:
+            line = f.read(16)
+            if not line:
+                print("EOF")
+                break
+            hcSanValA, canId, subId, timestamp, data, hcSanValB = parse_in(line)
+            print(hcSanValA, canId, subId, timestamp, data, hcSanValB)
+            # row = [hcSanValA, canId, subId, timestamp, data, hcSanValB]
     return
 
 def read_from_arduino(port_name, baud_rate):
@@ -44,8 +63,8 @@ def read_from_arduino(port_name, baud_rate):
                 print("waiting on line - empty")
                 continue
             try:
-                dev_addr, typ, time, data, p = parse_in(line)
-                print(dev_addr, typ, time, data, p)
+                hcSanValA, canId, subId, timestamp, data, hcSanValB = parse_in(line)
+                print(hcSanValA, canId, subId, timestamp, data, hcSanValB)
             except Exception as e:
                 print("Bad line")
                 continue
@@ -57,8 +76,8 @@ def read_from_arduino_v2(port_name, baud_rate):
     while True:
         while ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
-            dev_addr, typ, time, data, p = parse_in(line)
-            entry = [dev_addr, typ, time, data, p]
+            hcSanValA, canId, subId, timestamp, data, hcSanValB = parse_in(line)
+            entry = [hcSanValA, canId, subId, timestamp, data, hcSanValB]
             gv.buffer.put(entry)
         time.sleep(0.1)
     # Add error checks for port etc
